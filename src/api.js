bf16642025c4fb1a4742fb0782a790ebac226deb
Emerson <emerapmelo@gmail.com>
2022-07-13 19:17:58 -0300

    Requisito 3 concluído

--- src/api.js
+++ src/api.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
-// ...
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+const apiRoutes = express.Router();
+
+apiRoutes.post('/login', routes.login);
+
+app.use(apiRoutes);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,11 +1,11 @@
 const createUser= (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-      id: DataTypes.INTEGER,
       displayName: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       image: DataTypes.STRING,
     }, { 
+        timestamps: false,
         tableName: 'Users'
     });
   
